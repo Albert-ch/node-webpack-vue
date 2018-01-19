5b5dbe68e61c5fc048f5c77d7d126be058db670b
cheyao005 <chenyao005@lianjia.com>
2018-01-19 15:40:46 +0800

    package: mysql -> promise-mysql

--- server/middleware/CommonController.js
+++ server/middleware/CommonController.js
@@ -1,23 +1,36 @@
 const axios = require('axios')
-const mysql = require('mysql')
+const mysql = require('promise-mysql')
+
+const mysql_set = require('../config/mysql_info')
 
 module.exports = async (ctx)=> {
 
-    let connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '123456',
-        database: 'node-test'
-    })
-    connection.connect()
-    await new Promise( (resolve, reject)=> {
-        connection.query('select * from user where id = ?', 2 ,(error, results, fields)=>{
-            resolve( results[0] )
-        })
-        connection.end();
-
-    }).then( (data)=> {
-        ctx.body = data
+    await mysql.createConnection( mysql_set ).then( (conn) => {
+        let result = conn.query('select * from user where id = ?',2)
+        conn.end()
+        return result
+    } ).then( row => {
+        console.log( 'row->', row )
+        ctx.body = row
     } )
+
+
+
+    // let connection = mysql.createConnection({
+    //     host: 'localhost',
+    //     user: 'root',
+    //     password: '123456',
+    //     database: 'node-test'
+    // })
+    // connection.connect()
+    // await new Promise( (resolve, reject)=> {
+    //     connection.query('select * from user where id = ?', 2 ,(error, results, fields)=>{
+    //         resolve( results[0] )
+    //     })
+    //     connection.end();
+    //
+    // }).then( (data)=> {
+    //     ctx.body = data
+    // } )
 
 }
