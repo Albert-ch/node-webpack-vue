8abfb87d19aee0482ae0407a8764c5fc44914057
cheyao005 <chenyao005@lianjia.com>
2018-04-10 13:32:51 +0800

    引入拖拽

---
+++ client/page/drag/drag.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <ul>
+            <draggable v-model="list" :move="moveData" @update="updateData">
+                <li v-for="item,idx in list" :key="idx">
+                    {{ item.title }}
+                </li>
+            </draggable>
+            <input type="text" v-model="val" v-focus>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import draggable from 'vuedraggable'
+
+    export default {
+        components: {draggable},
+        directives: {
+            focus: {
+                // 指令的定义
+                inserted: function (el) {
+                    el.focus()
+                }
+            }
+        },
+        data() {
+            return {
+                list: [
+                    {
+                        title: '你来打我呀',
+                    },
+                    {
+                        title: '就不告诉你',
+                    },
+                    {
+                        title: 'Hello world',
+                    },
+                ],
+                val: '请输入'
+            }
+        },
+        watch:{
+            list(val){
+                console.log(val)
+            }
+        },
+        methods:{
+            moveData(event){
+                console.log( 'move', event )
+            },
+            updateData(event){
+                console.log('update', event)
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
--- client/router/index.js
+++ client/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // import axios from 'axios'
 import index from '../page/index/index.vue'
+import drag from '../page/drag/drag.vue'
 
 Vue.use(Router)
 
@@ -12,6 +13,11 @@ export default new Router({
             path: '/',
             name: 'index',
             component: index
+        },
+        {
+            path: '/drag',
+            name: 'drag',
+            component: drag
         }
     ]
 })
